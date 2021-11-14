1f88eb03b1463c60b2c10130ed4ea23373994054
pta200 <pta200@gmail.com>
2021-11-13 22:34:38 -0500

    add JWT auth

--- apis.py
+++ apis.py
@@ -1,13 +1,16 @@
 from flask import request
 from flask_restful import Resource
 from models import db, ma, Post, PostSchema, uuid
+from credentials import jwt_required
 
 class PostListResource(Resource):
     # list all posts
+    @jwt_required()
     def get(self):
         posts = Post.query.all()
         return posts_schema.dump(posts)
     # create new post
+    @jwt_required()
     def post(self):
         new_post = Post(
             post_id=uuid.uuid4(),
@@ -20,11 +23,13 @@ class PostListResource(Resource):
 
 class PostResource(Resource):
     # get post by id
+    @jwt_required()
     def get(self, post_id):
         post = Post.query.get_or_404(post_id)
         return post_schema.dump(post)
     
     # update post
+    @jwt_required()
     def patch(self, post_id):
         post = Post.query.get_or_404(post_id)
 
@@ -37,6 +42,7 @@ class PostResource(Resource):
         return post_schema.dump(post)
 
     # delete post
+    @jwt_required()
     def delete(self, post_id):
         post = Post.query.get_or_404(post_id)
         db.session.delete(post)
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask
-from models import db, ma, Post, PostSchema
-from apis import PostResource, PostListResource
+from models import db, ma, Post, PostSchema, RevokedTokenModel
+#from apis import PostResource, PostListResource
+import apis, credentials
 from flask_restful import Api
+from flask_jwt_extended import JWTManager
+
 
 # init app
 app = Flask(__name__)
@@ -13,12 +16,34 @@ db.app = app
 db.init_app(app)
 ma.init_app(app)
 
+# init jwt
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+jwt = JWTManager(app)
+
+# check it token is on blacklist
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blacklist(header,decrypted_token):
+    jti = decrypted_token['jti']
+    return RevokedTokenModel.is_jti_blacklisted(jti)
+
 # init flask restful
 api = Api(app)
 
 # register apis
-api.add_resource(PostListResource, '/posts')
-api.add_resource(PostResource, '/posts/<int:post_id>')
+api.add_resource(apis.PostListResource, '/posts')
+api.add_resource(apis.PostResource, '/posts/<int:post_id>')
+
+# regiser jtw
+api.add_resource(credentials.UserRegistration, '/registration')
+api.add_resource(credentials.UserLogin, '/login')
+api.add_resource(credentials.UserLogoutAccess, '/logout/access')
+api.add_resource(credentials.UserLogoutRefresh, '/logout/refresh')
+api.add_resource(credentials.TokenRefresh, '/token/refresh')
+api.add_resource(credentials.AllUsers, '/users')
+#api.add_resource(credentials.SecretResource, '/secret')
 
 if __name__=='__main__':
     app.run(debug=True)
