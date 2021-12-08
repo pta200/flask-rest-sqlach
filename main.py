b93016fd2d797b6bad0ed20f864dca729021d4f9
pta200 <pta200@gmail.com>
2021-12-07 19:34:31 -0500

    Add tests and application factory

--- main.py
+++ main.py
@@ -5,46 +5,49 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 
+# app factory
+def create_app(test_config=None):
+    # init app
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# init app
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # init sqlalchemy and marshmallow
+    db.app = app
+    db.init_app(app)
+    ma.init_app(app)
 
-# init sqlalchemy and marshmallow
-db.app = app
-db.init_app(app)
-ma.init_app(app)
+    # init jwt
+    app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+    app.config['JWT_BLACKLIST_ENABLED'] = True
+    app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+    app.config['PROPAGATE_EXCEPTIONS'] = True
 
-# init jwt
-app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
-app.config['JWT_BLACKLIST_ENABLED'] = True
-app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
-app.config['PROPAGATE_EXCEPTIONS'] = True
+    # regiser jtw
+    jwt = JWTManager(app)
 
-# regiser jtw
-jwt = JWTManager(app)
+    # check it token is on blacklist
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blacklist(header,decrypted_token):
+        jti = decrypted_token['jti']
+        return RevokedTokenModel.is_jti_blacklisted(jti)
 
-# check it token is on blacklist
-@jwt.token_in_blocklist_loader
-def check_if_token_in_blacklist(header,decrypted_token):
-    jti = decrypted_token['jti']
-    return RevokedTokenModel.is_jti_blacklisted(jti)
+    # init flask restful
+    api = Api(app)
+    CORS(app)
 
-# init flask restful
-api = Api(app)
-CORS(app)
-
-# register apis
-api.add_resource(apis.PostListResource, '/posts')
-api.add_resource(apis.PostResource, '/posts/<int:post_id>')
-api.add_resource(credentials.UserRegistration, '/registration')
-api.add_resource(credentials.UserLogin, '/login')
-api.add_resource(credentials.UserLogoutAccess, '/logout/access')
-api.add_resource(credentials.UserLogoutRefresh, '/logout/refresh')
-api.add_resource(credentials.TokenRefresh, '/token/refresh')
-api.add_resource(credentials.AllUsers, '/users')
-#api.add_resource(credentials.SecretResource, '/secret')
+    # register apis
+    api.add_resource(apis.PostListResource, '/posts')
+    api.add_resource(apis.PostResource, '/posts/<int:post_id>')
+    api.add_resource(credentials.UserRegistration, '/registration')
+    api.add_resource(credentials.UserLogin, '/login')
+    api.add_resource(credentials.UserLogoutAccess, '/logout/access')
+    api.add_resource(credentials.UserLogoutRefresh, '/logout/refresh')
+    api.add_resource(credentials.TokenRefresh, '/token/refresh')
+    api.add_resource(credentials.AllUsers, '/users')
+    #api.add_resource(credentials.SecretResource, '/secret')
+    return app
 
 if __name__=='__main__':
+    app = create_app()
     app.run(debug=True)
---
+++ test_apis.py
@@ -0,0 +1,22 @@
+import pytest, json
+from main import create_app
+
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config["TESTING"] = True
+
+    with app.test_client() as client:
+        yield client
+
+    #os.close(db_fd)
+    #os.unlink(db_path)
+
+def test_users(client):
+    response = client.get('/users')
+    assert response.status_code == 200
+    json_data = json.loads(response.data)
+    assert 'users' in json_data
+    assert len(json_data['users']) > 0
+   
